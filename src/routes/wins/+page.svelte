69a74d4caea0ddaa57ed3ab0f527cc67627aeec1
Evan Yip <etyip@ucsb.edu>
2023-05-31 20:53:50 -0700

    Fix wording on delete button

--- src/routes/wins/+page.svelte
+++ src/routes/wins/+page.svelte
@@ -61,7 +61,7 @@
 							class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
 							on:click={() => handleDelete(index)}
 						>
-							Delete (replace s)
+							Delete
 						</button>
 					</div>
 				</li>
